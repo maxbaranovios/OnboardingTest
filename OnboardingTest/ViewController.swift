35882aea7e4a518ad9fd649f926284c8e9fcd833
Maxim <45602494+fourfifteen@users.noreply.github.com>
2022-12-05 11:07:47 +0100

    Initial Commit

---
+++ OnboardingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingTest
+//
+//  Created by Maxim Baranov on 5.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
